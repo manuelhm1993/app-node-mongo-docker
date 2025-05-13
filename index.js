590bbc5db9ac9b703aa5d49ca50b2baca1816508
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-13 16:42:43 -0400

    Modificación exitosa y configuración del entorno con docker

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from "express";
+import mongoose from "mongoose";
 
-const Animal = mongoose.model('Animal', new mongoose.Schema({
-  tipo: String,
-  estado: String,
-}))
+const Animal = mongoose.model(
+    "Animal",
+    new mongoose.Schema({
+        tipo: String,
+        estado: String,
+    })
+);
 
-const app = express()
+const app = express();
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect(
+    "mongodb://mhenriquez:123456@localhost:27017/miapp?authSource=admin"
+);
 
-app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
-  const animales = await Animal.find();
-  return res.send(animales)
-})
-app.get('/crear', async (_req, res) => {
-  console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
-  return res.send('ok')
-})
+app.get("/", async (_req, res) => {
+    console.log("listando... chanchitos...");
+    const animales = await Animal.find();
+    return res.send(animales);
+});
+app.get("/crear", async (_req, res) => {
+    console.log("creando...");
+    await Animal.create({ tipo: "Chanchito", estado: "Feliz" });
+    return res.send("ok");
+});
 
-app.listen(3000, () => console.log('listening...'))
+app.listen(3000, () => console.log("listening..."));
